7c445aaab3ec7935c2b733232531cbe584550503
Kalun Lau <klnlau@gmail.com>
2018-10-15 12:10:40 -0500

    Blink test with 4 modes
    Using fablab PCB running on 20MHz crystal resonator

---
+++ 15102018_attiny44fab_blink/15102018_attiny44fab_blink.ino
@@ -0,0 +1,75 @@
+
+//Declaración de variables
+int buttonState;
+int lastButtonState = LOW;
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;
+int modeFlash = 0;
+
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(8, OUTPUT);
+  pinMode(3, INPUT);
+}
+
+void loop() {
+  int reading = digitalRead(3);
+
+  if (reading != lastButtonState){
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay){
+    if(reading != buttonState){
+      buttonState = reading;
+      if(buttonState == HIGH){
+        modeFlash = modeFlash + 1;
+        if (modeFlash == 5){
+          modeFlash = 0;
+        }
+      }
+    }
+  }
+  switch (modeFlash){
+    case 0:
+      digitalWrite(8, HIGH);   // turn the LED on (HIGH is the voltage level)
+      delay(50);               // wait for a second
+      digitalWrite(8, LOW);    // turn the LED off by making the voltage LOW
+      delay(1000);               // wait for a second
+      break;
+    case 1:
+      digitalWrite(8, HIGH);   // turn the LED on (HIGH is the voltage level)
+      delay(50);              // wait for a second
+      digitalWrite(8, LOW);   // turn the LED on (HIGH is the voltage level)
+      delay(100);              // wait for a second
+      digitalWrite(8, HIGH);   // turn the LED on (HIGH is the voltage level)
+      delay(50);              // wait for a second
+      digitalWrite(8, LOW);    // turn the LED off by making the voltage LOW
+      delay(1500);              // wait for a second
+      break;
+    case 2:
+      digitalWrite(8, HIGH);   // turn the LED on (HIGH is the voltage level)
+      delay(100);              // wait for a second
+      digitalWrite(8, LOW);    // turn the LED off by making the voltage LOW
+      delay(1500);              // wait for a second
+      break;
+    case 3:
+      digitalWrite(8, HIGH);   // turn the LED on (HIGH is the voltage level)
+      delay(100);              // wait for a second
+      digitalWrite(8, LOW);   // turn the LED on (HIGH is the voltage level)
+      delay(200);              // wait for a second
+      digitalWrite(8, HIGH);   // turn the LED on (HIGH is the voltage level)
+      delay(100);              // wait for a second
+      digitalWrite(8, LOW);    // turn the LED off by making the voltage LOW
+      delay(1500);              // wait for a second
+      break;
+    case 4:
+      digitalWrite(8, HIGH);   // turn the LED on (HIGH is the voltage level)
+      delay(200);              // wait for a second
+      digitalWrite(8, LOW);    // turn the LED off by making the voltage LOW
+      delay(2000);              // wait for a second
+      break;
+  }
+  lastButtonState = reading;
+}
